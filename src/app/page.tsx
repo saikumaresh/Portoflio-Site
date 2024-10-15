45b2c296dd9a4a72e50805122417a58dd4bc0b3f
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-15 23:06:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,31 +50,31 @@ export default function Page() {
         </BlurFade>
       </section>
       <section id="work">
-        <div className="flex min-h-0 flex-col gap-y-3">
-          <BlurFade delay={BLUR_FADE_DELAY * 5}>
-            <h2 className="text-xl font-bold">Work Experience</h2>
-          </BlurFade>
-          {DATA.work.map((work, id) => (
-            <BlurFade
+      <div className="flex min-h-0 flex-col gap-y-3">
+        <BlurFade delay={BLUR_FADE_DELAY * 5}>
+          <h2 className="text-xl font-bold">Work Experience</h2>
+        </BlurFade>
+        {DATA.work.map((work, id) => (
+          <BlurFade
+            key={work.company}
+            delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+          >
+            <ResumeCard
               key={work.company}
-              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
-            >
-              <ResumeCard
-                key={work.company}
-                logoUrl={work.logoUrl}
-                altText={work.company}
-                title={work.company}
-                subtitle={work.title}
-                href={work.href}
-                badges={work.badges}
-                period={`${work.start} - ${work.end ?? "Present"}`}
-                description={work.description.split('\n').map((line, index) => (
-                  <div key={index}>{line.trim()}</div>
-                ))}       
-              />
-            </BlurFade>
-          ))}
-        </div>
+              logoUrl={work.logoUrl}
+              altText={work.company}
+              title={work.company}
+              subtitle={work.title}
+              href={work.href}
+              badges={work.badges}
+              period={`${work.start} - ${work.end ?? "Present"}`}
+              description={work.description.split('\n').map((line, index) => (
+                <div key={index}>{line.trim()}</div>
+              ))}       
+            />
+          </BlurFade>
+        ))}
+      </div>
       </section>
       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
