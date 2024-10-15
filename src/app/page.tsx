e7873267f6047d712af990e0277a7282f686a46f
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-15 22:38:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,6 @@ export default function Page() {
                 href={work.href}
                 badges={work.badges}
                 period={`${work.start} - ${work.end ?? "Present"}`}
-                //description={work.description}
                 description={work.description.split('\n').map((line, index) => (
                   <div key={index}>{line}</div>
                 ))}
