c55e9699af1e4fd0c5d98bec2e54ca758ed15fa5
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-18 00:26:24 +0530

    automation

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -146,7 +146,7 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "/networkautomation.jpeg",
+      image: "/automation.gif",
       video:"",
     },
     {
