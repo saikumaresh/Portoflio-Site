4a544f74aa2d573de17d8669715b3c4105d60bd5
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-18 13:37:04 +0530

    Update project-card.tsx

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -43,7 +43,7 @@ export function ProjectCard({
   return (
     <Card
       className={
-        "flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full"
+        "flex flex-col overflow-hidden border-2 hover:shadow-lg transition-all duration-300 ease-out h-full"
       }
     >
       <Link
